087acaa53c432ab03efd745c238c9154f2cadf07
Garrett <1523408280@mil>
2019-02-22 11:33:33 -0500

    Almost done

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.template import loader
